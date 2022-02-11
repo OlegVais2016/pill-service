c432024c8a2775190934fdad31656eb8f1f38359
Oleg Vaiser <aly9681082@gmail.com>
2022-02-11 11:43:36 +0200

    sec

---
+++ src/main/java/com/example/pillservice/PillServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.pillservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PillServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PillServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pillservice/PillServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pillservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PillServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
